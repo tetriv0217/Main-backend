cdcd26a3ee6e8af96634c6ca5502c4937cb28cb1
tetriv0217 <tetriv0217@gmail.com>
2024-06-10 21:25:37 +0530

    made some comments change

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async ()=>{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDb connected!! DB host: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGO DB Connection error",error);
+        console.log("MONGO DB Connection error!!!",error);
         process.exit(1)
     }
 }
